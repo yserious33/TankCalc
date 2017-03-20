377bf36df9fd1e16548034809708cd940c83ab76
Allyson DeSantos <yserious33@aol.com>
2017-03-20 16:07:47 -0400

    Added application files

---
+++ www/js/app.js
@@ -0,0 +1,47 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('starter', ['ionic'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+
+      // Don't remove this line unless you know what you are doing. It stops the viewport
+      // from snapping when text inputs are focused. Ionic handles this internally for
+      // a much nicer keyboard experience.
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.controller('TestCtrl', ['$scope',
+  function($scope){
+  $scope.gal= function(){
+  var len = $scope.len;
+  var hei = $scope.hei;
+  var dep = $scope.dep;
+
+  if( len > 0 && hei > 0 && dep > 0){
+    return Math.round((hei * dep * len)*0.00433);
+  }
+}
+
+$scope.light = function(){
+
+  var len = $scope.len;
+  
+
+  if( len > 0 ){
+    return Math.round(len/12);
+  }
+  }
+}]);
